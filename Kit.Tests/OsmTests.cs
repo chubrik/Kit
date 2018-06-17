28725e8c8115741dc1c76db856f827fb3371ce20
Arkadi Chubrik <arkadi@chubrik.org>
2018-06-18 01:00:29 +0300

    Kit.Osm project

---
+++ Kit.Tests/OsmTests.cs
@@ -0,0 +1,16 @@
+﻿using Kit.Osm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kit.Tests
+{
+    [TestClass]
+    public class OsmTests : TestsBase
+    {
+        [TestMethod]
+        public void Validate()
+        {
+            TestInitialize($"{GetType().Name}.{nameof(Validate)}");
+            OsmService.ValidateSource("../../$data/antarctica.osm.pbf");
+        }
+    }
+}
