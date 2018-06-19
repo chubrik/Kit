c6cc84495a03dd3e91c259d0783d7559f81e4952
Arkadi Chubrik <arkadi@chubrik.org>
2018-06-19 18:24:52 +0300

    Osm test: get by predicate

--- Kit.Tests/OsmTests.cs
+++ Kit.Tests/OsmTests.cs
@@ -6,11 +6,27 @@ namespace Kit.Tests
     [TestClass]
     public class OsmTests : TestsBase
     {
+        private const string SrcPath = "../../$data/antarctica.osm.pbf";
+
         [TestMethod]
         public void Validate()
         {
             TestInitialize($"{GetType().Name}.{nameof(Validate)}");
-            OsmService.Validate("../../$data/antarctica.osm.pbf");
+            OsmService.Validate(SrcPath);
+        }
+
+        [TestMethod]
+        public void Predicate()
+        {
+            TestInitialize($"{GetType().Name}.{nameof(Predicate)}");
+            var response = OsmService.Load(SrcPath, i => i.Tags.ContainsKey("place"));
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Nodes.Count > 100);
+            Assert.IsTrue(response.Ways.Count > 5000);
+            Assert.IsTrue(response.Relations.Count > 1000);
+            Assert.IsTrue(response.MissedNodeIds.Count == 0);
+            Assert.IsTrue(response.MissedWayIds.Count == 0);
+            Assert.IsTrue(response.MissedRelationIds.Count == 0);
         }
     }
 }
