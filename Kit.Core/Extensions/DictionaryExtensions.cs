d70d7ab2d8e94bdf26a8835529c4712294efa326
Arkadi Chubrik <arkadi@chubrik.org>
2020-03-01 00:31:41 +0300

    1.6.3

--- Kit.Core/Extensions/DictionaryExtensions.cs
+++ Kit.Core/Extensions/DictionaryExtensions.cs
@@ -4,13 +4,6 @@ namespace Kit
 {
     public static class DictionaryExtensions
     {
-        public static TValue GetValueOrDefault<TKey, TValue>(
-            this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
-        {
-            dictionary.TryGetValue(key, out TValue value);
-            return value;
-        }
-
         public static bool Contains<TKey, TValue>(
             this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, TValue value) =>
             dictionary.TryGetValue(key, out var i) && i.Equals(value);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [![Kit project](https://raw.githubusercontent.com/chubrik/Kit/master/logo-64x64.png)](#) Kit
+# [![Kit project](https://raw.githubusercontent.com/chubrik/Kit/master/icon.png)](#) Kit
 [![NuGet](https://img.shields.io/nuget/v/Kit.svg)](https://www.nuget.org/packages/Kit/) [![Build Status](https://travis-ci.org/chubrik/Kit.svg?branch=master)](https://travis-ci.org/chubrik/Kit)
 
 The powerful engine for various C# projects.<br>Has advanced diagnostics and the most useful features.
